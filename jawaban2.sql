b8a371822a450d34396dcb1187581034cfe0fa66
adinda1721 <129568926+adinda1721@users.noreply.github.com>
2023-12-03 18:47:57 +0700

    Create jawaban2.sql

---
+++ jawaban2.sql
@@ -0,0 +1,2 @@
+INSERT INTO Mahasiswa (nim,f_name, m_name, l_name, kd_matkul) VALUE (123, Adinda, Asri, Azizah, 1);
+INSERT INTO Dosen (nim,f_name, m_name, l_name, kd_matkul) VALUE (456, Jefry, Sunupurwa, Asri, 1);
